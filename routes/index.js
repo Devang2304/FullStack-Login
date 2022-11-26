5afb8c9b28cfa7c4fa54a19b4f653166a0f1a6c1
Devang2304 <devangvartak2304@gmail.com>
2022-11-27 01:57:55 +0530

    Added get method to render the dashboard

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,7 @@ const router =express.Router();
 router.get('/',(req,res)=>{
     res.render('welcome');
 });
+//dashboard page
+router.get('/dashboard',(req,res)=>res.render('dashboard'));
 
 module.exports = router;
