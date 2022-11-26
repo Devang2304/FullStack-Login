c4621287f5c6096523b6c8117269745c468adb89
Devang2304 <devangvartak2304@gmail.com>
2022-11-26 23:51:58 +0530

    Created server and other stuff

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
 
 const app = express();
 
+const db=require('./config/keys').MongoURI;
+
+//connecting to Mongo
+mongoose.connect(db, {useNewUrlParser: true})
+.then(() => {
+    console.log('MongoDB Connected...')
+}).catch((err) => {
+    console.log(err);
+});
 
 //ejs
 app.use(expressLayouts);
 app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({extended:false}));
 
 // Routes
 app.use('/',require('./routes/index'));
