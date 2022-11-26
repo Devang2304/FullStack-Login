96cae2bf7866a9b5d8cd3041bcbef7caade5b6cd
Devang2304 <devangvartak2304@gmail.com>
2022-11-27 01:56:35 +0530

    imported whatever was required

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
+const session= require('express-session');
+const flash = require('connect-flash');
+const passport = require('passport');
 
 const app = express();
+
+require('./config/passport')(passport);
 
 const db=require('./config/keys').MongoURI;
 
@@ -19,6 +24,25 @@ app.use(expressLayouts);
 app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({extended:false}));
+
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true }
+  }));
+
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+app.use(flash());
+
+app.use((req,res,next)=>{
+    res.locals.success_msg=req.flash('success_msg');
+    res.locals.error_msg=req.flash('error_msg');
+    res.locals.error=req.flash('error');
+    next();
+});
 
 // Routes
 app.use('/',require('./routes/index'));
